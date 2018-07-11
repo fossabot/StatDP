36c52e2da4f54a5258c7e0e1dd6b5ac958bbc8bb
Ryan Wang <ryanwang.cs@gmail.com>
2018-07-10 20:13:31 -0400

    Fix math.inf problem which is not supported until python 3.5

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -1,10 +1,9 @@
 from statdp.algorithms import *
-import math
 
 
 def test_noisymax():
     # add no noise to the array
-    assert noisy_max_v1a([1, 2, 1], math.inf) == 1
-    assert noisy_max_v1b([1, 3, 1], math.inf) == 3
-    assert noisy_max_v2a([1, 3, 1], math.inf) == 1
-    assert noisy_max_v2b([1, 3, 1], math.inf) == 3
+    assert noisy_max_v1a([1, 2, 1], float('inf')) == 1
+    assert noisy_max_v1b([1, 3, 1], float('inf')) == 3
+    assert noisy_max_v2a([1, 3, 1], float('inf')) == 1
+    assert noisy_max_v2b([1, 3, 1], float('inf')) == 3
