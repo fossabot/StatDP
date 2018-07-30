d02a40996fe76cd0a1e2a5d253c31bfde1d2a203
ppht <edgw564731@yahoo.com>
2018-07-30 13:29:21 -0400

    add laplace mechanism into algorithm list

--- statdp/algorithms.py
+++ statdp/algorithms.py
@@ -142,3 +142,13 @@ def sparse_vector_4(Q, eps, N, T):
 
     true_count = int(len(Q) / 2)
     return np.count_nonzero(out != ([True for _ in range(true_count)] + [False for _ in range(len(Q) - true_count)]))
+
+def laplace_mechanism(Q, eps):
+    noisy_array = [a + np.random.laplace(scale=len(Q)/eps) for a in Q]
+    floor=1-0.27
+    ceiling=1+0.75
+    count=0
+    for i in noisy_array:
+        if i>=floor and i <=ceiling:
+            count+=1
+    return count
