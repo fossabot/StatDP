9971c2d9d1befef3bc157e6febd7b96fad374af2
Ryan Wang <ryanwang.cs@gmail.com>
2018-08-29 23:00:43 -0400

    The system now only takes kwargs for algorithm.

--- tests/test_core.py
+++ tests/test_core.py
@@ -6,13 +6,13 @@ def test_core_single():
     D1 = [0] + [2 for _ in range(4)]
     D2 = [1 for _ in range(5)]
     event = [0]
-    p1, p2 = hypothesis_test(noisy_max_v1a, (), {'eps': 0.5}, D1, D2, event, 0.25, 100000, cores=1)
+    p1, p2 = hypothesis_test(noisy_max_v1a, {'eps': 0.5}, D1, D2, event, 0.25, 100000, cores=1)
     assert 0 <= p1 <= 0.05
     assert 0.95 <= p2 <= 1.0
-    p1, p2 = hypothesis_test(noisy_max_v1a, (), {'eps': 0.5}, D1, D2, event, 0.5, 100000, cores=1)
+    p1, p2 = hypothesis_test(noisy_max_v1a, {'eps': 0.5}, D1, D2, event, 0.5, 100000, cores=1)
     assert 0.05 <= p1 <= 1.0
     assert 0.95 <= p2 <= 1.0
-    p1, p2 = hypothesis_test(noisy_max_v1a, (), {'eps': 0.5}, D1, D2, event, 0.75, 100000, cores=1)
+    p1, p2 = hypothesis_test(noisy_max_v1a, {'eps': 0.5}, D1, D2, event, 0.75, 100000, cores=1)
     assert 0.95 <= p1 <= 1.0
     assert 0.95 <= p2 <= 1.0
 
@@ -21,13 +21,13 @@ def test_core_multi():
     D1 = [0] + [2 for _ in range(4)]
     D2 = [1 for _ in range(5)]
     event = [0]
-    p1, p2 = hypothesis_test(noisy_max_v1a, (), {'eps': 0.5}, D1, D2, event, 0.25, 100000, cores=0)
+    p1, p2 = hypothesis_test(noisy_max_v1a, {'eps': 0.5}, D1, D2, event, 0.25, 100000, cores=0)
     assert 0 <= p1 <= 0.05
     assert 0.95 <= p2 <= 1.0
-    p1, p2 = hypothesis_test(noisy_max_v1a, (), {'eps': 0.5}, D1, D2, event, 0.5, 100000, cores=0)
+    p1, p2 = hypothesis_test(noisy_max_v1a, {'eps': 0.5}, D1, D2, event, 0.5, 100000, cores=0)
     assert 0.05 <= p1 <= 1.0
     assert 0.05 <= p1 <= 1.0
     assert 0.95 <= p2 <= 1.0
-    p1, p2 = hypothesis_test(noisy_max_v1a, (), {'eps': 0.5}, D1, D2, event, 0.75, 100000, cores=0)
+    p1, p2 = hypothesis_test(noisy_max_v1a, {'eps': 0.5}, D1, D2, event, 0.75, 100000, cores=0)
     assert 0.95 <= p1 <= 1.0
     assert 0.95 <= p2 <= 1.0
