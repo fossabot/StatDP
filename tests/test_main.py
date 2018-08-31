98b44201eae07037389a42dc11ff2c1da04c305e
Ryan Wang <ryanwang.cs@gmail.com>
2018-08-31 11:07:36 -0400

    Fix argument name.

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,4 +3,4 @@ from statdp.algorithms import noisy_max_v1a
 
 
 def test_main():
-    assert detect_counter_example(noisy_max_v1a, 0.5, {'epsilon': 0.2}) >= 0.95
+    assert detect_counter_example(noisy_max_v1a, 0.5, {'eps': 0.2}) >= 0.95
