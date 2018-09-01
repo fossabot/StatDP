c10ec16621cb50c8a94c007f5d72eb0a0d008c5b
Ryan Wang <ryanwang.cs@gmail.com>
2018-09-01 07:56:35 -0400

    Change the input to 'epsilon'.

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,7 @@ from statdp.algorithms import noisy_max_v1a
 
 
 def test_main():
-    result = detect_counter_example(noisy_max_v1a, 0.5, {'eps': 0.2})
+    result = detect_counter_example(noisy_max_v1a, 0.5, {'epsilon': 0.2})
     assert isinstance(result, list) and len(result) == 1
     epsilon, p = result[0]
     assert epsilon == 0.5 and p >= 0.95
--- tests/test_selectors.py
+++ tests/test_selectors.py
@@ -5,8 +5,8 @@ from statdp.selectors import select_event
 def test_select_event():
     d1 = [0] + [2 for _ in range(4)]
     d2 = [1 for _ in range(5)]
-    _, _, _, event = select_event(noisy_max_v1a, [(d1, d2, {'eps': 0.5})], 0.5, 100000, cores=0)
+    _, _, _, event = select_event(noisy_max_v1a, [(d1, d2, {'epsilon': 0.5})], 0.5, 100000, cores=0)
     assert 0 in event
-    _, _, _, event = select_event(noisy_max_v1b, [(d1, d2, {'eps': 0.5})], 0.5, 100000, cores=0)
+    _, _, _, event = select_event(noisy_max_v1b, [(d1, d2, {'epsilon': 0.5})], 0.5, 100000, cores=0)
     assert 0 in event
 
