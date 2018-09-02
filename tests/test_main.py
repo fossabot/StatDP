ad50192542152d43d6f250da75da213d2c407488
Ryan Wang <ryanwang.cs@gmail.com>
2018-09-01 23:21:54 -0400

    Add more tests.

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,3 +7,6 @@ def test_main():
     assert isinstance(result, list) and len(result) == 1
     epsilon, p = result[0]
     assert epsilon == 0.5 and p >= 0.95
+    result = detect_counter_example(noisy_max_v1a, 0.2, {'epsilon': 0.5})
+    epsilon, p = result[0]
+    assert epsilon == 0.2 and p <= 0.05
